e7e26b9121805a5ab09a1589a1723c94888e609c
harshsri28 <66912920+harshsri28@users.noreply.github.com>
2021-01-27 00:55:22 +0530

    Add files via upload

---
+++ src/Login.js
@@ -0,0 +1,40 @@
+import { Button } from "@material-ui/core";
+import React from "react";
+import { useDispatch } from "react-redux";
+import { login } from "./features/userSlice";
+import { auth, provider } from "./Firebase";
+import "./Login.css";
+
+function Login() {
+  const dispatch = useDispatch();
+  const signIN = () => {
+    auth
+      .signInWithPopup(provider)
+      .then(({ user }) => {
+        dispatch(
+          login({
+            displayName: user.displayName,
+            email: user.email,
+            photoUrl: user.photoURL,
+          })
+        );
+      })
+      .catch((error) => alert(error.message));
+  };
+
+  return (
+    <div className="login">
+      <div className="login__container">
+        <img
+          src="https://cdn.vox-cdn.com/thumbor/Tbqi3ZF9Qz0fTJIUvkgQe3FdN0k=/1400x788/filters:format(jpeg)/cdn.vox-cdn.com/uploads/chorus_asset/file/21939811/newgmaillogo.jpg"
+          alt=""
+        />
+        <Button variant="contained" color="primary" onClick={signIN}>
+          Login
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
